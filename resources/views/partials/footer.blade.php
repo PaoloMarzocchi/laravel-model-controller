d168102fa72e0c7dfacbd4262f0c3b8894206489
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-05-09 17:57:07 +0200

    add footer and edit style in movies page

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,50 @@
-<footer>Footer</footer>
+<footer>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-4">
+            <div class="col">
+                <h5>Contacts</h5>
+                <ul class="list-unstyled">
+                    <li>Lorem.</li>
+                    <li>Eum.</li>
+                    <li>Nulla!</li>
+                    <li>Ut?</li>
+                    <li>Tempore.</li>
+                    <li>Sequi!</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h5>Legal</h5>
+                <ul class="list-unstyled">
+                    <li>Lorem.</li>
+                    <li>Eum.</li>
+                    <li>Nulla!</li>
+                    <li>Ut?</li>
+                    <li>Tempore.</li>
+                    <li>Sequi!</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h5>About us</h5>
+                <ul class="list-unstyled">
+                    <li>Lorem.</li>
+                    <li>Eum.</li>
+                    <li>Nulla!</li>
+                    <li>Ut?</li>
+                    <li>Tempore.</li>
+                    <li>Sequi!</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h5>Social</h5>
+                <ul class="list-unstyled">
+                    <li>Lorem.</li>
+                    <li>Eum.</li>
+                    <li>Nulla!</li>
+                    <li>Ut?</li>
+                    <li>Tempore.</li>
+                    <li>Sequi!</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
